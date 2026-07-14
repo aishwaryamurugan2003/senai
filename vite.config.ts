39f94d9cfc2dc608533a9ba6a4a2ae5f6441778c
hemnath0075 <hemnath5700@gmail.com>
2026-07-14 12:34:43 +0530

    vite config changed

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/senai/",
+  base: "https://main.dpwqb9kzfy4tu.amplifyapp.com/senai/",
 })
